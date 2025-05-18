bfa1258f72af63d6c1afd45478ce4ec4a68437a0
mkadochnikov <47840570+mkadochnikov@users.noreply.github.com>
2025-05-18 17:21:33 +0300

    Поменял скрипт запуска 6

--- run.sh
+++ run.sh
@@ -24,7 +24,7 @@ restart_streamlit() {
 
     echo "Запускаю Streamlit..."
     cd "$STREAMLIT_DIR" || exit
-    nohup streamlit run app.py --server.port $STREAMLIT_PORT > streamlit.log 2>&1 &
+    nohup streamlit run streamlit_app.py --server.port $STREAMLIT_PORT > streamlit.log 2>&1 &
     echo "Streamlit запущен на порту $STREAMLIT_PORT"
 }
 
